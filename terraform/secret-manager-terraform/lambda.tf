46ef44127e2a3910f6092bf8e87cda39690f17f4
Lokurte, Rahul <rahul.m.lokurte@gmail.com>
2021-07-30 18:13:15 +0530

    Secret Manager Terraform

---
+++ terraform/secret-manager-terraform/lambda.tf
@@ -0,0 +1,18 @@
+module "profile_generator_lambda" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "2.7.0"
+  # insert the 28 required variables here
+  function_name = "profile-generator-lambda"
+  description   = "Generates a new profiles"
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  source_path   = "${path.module}/resources/profile-generator-lambda"
+  layers        = [module.profile_generator_lambda_layer.lambda_layer_arn]
+  environment_variables = {
+    "mongoURI"       = data.aws_secretsmanager_secret_version.secret_credentials.secret_string
+  }
+
+  tags = {
+    Name = "profile-generator-lambda"
+  }
+}
---
+++ terraform/secret-manager-terraform/sources.tf
@@ -0,0 +1,7 @@
+data "aws_secretsmanager_secret" "secret_name" {
+   name = "mongodbURI"
+}
+
+data "aws_secretsmanager_secret_version" "secret_credentials" {
+  secret_id = data.aws_secretsmanager_secret.secret_name.id
+}
