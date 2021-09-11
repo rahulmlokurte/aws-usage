60df8e93306495d97326d255a5b86847bf27c26e
Lokurte, Rahul <rahul.m.lokurte@gmail.com>
2021-09-12 00:08:20 +0530

    Adding VPC through terraform

---
+++ terraform/vpc-learning/vpc.tf
@@ -0,0 +1,56 @@
+resource "aws_vpc" "vpc-learning" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "vpc-learning"
+  }
+}
+
+resource "aws_subnet" "vpc-learning-public-subnet-1" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.vpc-learning.id
+  map_public_ip_on_launch = true
+  availability_zone = "ap-south-1"
+}
+
+resource "aws_internet_gateway" "vpc-learning-internet-gateway" {
+  vpc_id = aws_vpc.vpc-learning.id
+}
+
+resource "aws_route_table" "vpc-learning-public-route-table" {
+  vpc_id = aws_vpc.vpc-learning.id
+}
+
+resource "aws_route" "vpc-learning-public-route" {
+  route_table_id = aws_route_table.vpc-learning-public-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.vpc-learning-internet-gateway.id
+}
+
+resource "aws_route_table_association" "vpc-learning-public-route-table-association" {
+  route_table_id = aws_route_table.vpc-learning-public-route-table.id
+  subnet_id = aws_subnet.vpc-learning-public-subnet-1.id
+}
+
+resource "aws_security_group" "vpc-learning-security-group" {
+  name = "vpc-learning-default-security-group"
+  vpc_id = aws_vpc.vpc-learning.id
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = [var.ingress_egress_cidr_blocks]
+  }
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = [var.ingress_egress_cidr_blocks]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = [var.ingress_egress_cidr_blocks]
+  }
+}
