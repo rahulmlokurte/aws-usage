1d3d256a094d59ccf9b4eea84c11362e0ba295d2
Lokurte, Rahul <rahul.m.lokurte@gmail.com>
2021-09-12 01:19:11 +0530

    Changing the availability zone

--- terraform/vpc-learning/vpc.tf
+++ terraform/vpc-learning/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "vpc-learning-public-subnet-1" {
   cidr_block = "10.0.1.0/24"
   vpc_id = aws_vpc.vpc-learning.id
   map_public_ip_on_launch = true
-  availability_zone = "ap-south-1"
+  availability_zone = "ap-south-1a"
 }
 
 resource "aws_internet_gateway" "vpc-learning-internet-gateway" {
