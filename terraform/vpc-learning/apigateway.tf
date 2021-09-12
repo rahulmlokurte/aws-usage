e92287e9adc8b10531e5f4fcffa08f2e1b5b27cd
Lokurte, Rahul <rahul.m.lokurte@gmail.com>
2021-09-13 00:57:19 +0530

    Adding aws_api_gateway_integration_response

--- terraform/vpc-learning/apigateway.tf
+++ terraform/vpc-learning/apigateway.tf
@@ -54,6 +54,35 @@ resource "aws_api_gateway_integration" "api-gateway-vpc-learning-integration" {
   resource_id = aws_api_gateway_resource.api-gateway-vpc-learning-resources.id
   rest_api_id = aws_api_gateway_rest_api.api-gateway-vpc-learning.id
   type = "MOCK"
+  request_templates = {
+    "application/json": "{\"statusCode\": 200}"
+  }
+  depends_on = [aws_api_gateway_method.api-gateway-vpc-learning-method]
+}
+
+resource "aws_api_gateway_method_response" "api-gateway-vpc-learning-response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api-gateway-vpc-learning.id
+  resource_id = aws_api_gateway_resource.api-gateway-vpc-learning-resources.id
+  http_method = aws_api_gateway_method.api-gateway-vpc-learning-method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "api-gateway-vpc-learning-IntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.api-gateway-vpc-learning.id
+  resource_id = aws_api_gateway_resource.api-gateway-vpc-learning-resources.id
+  http_method = aws_api_gateway_method.api-gateway-vpc-learning-method.http_method
+  status_code = aws_api_gateway_method_response.api-gateway-vpc-learning-response_200.status_code
+
+  # Transforms the backend JSON response to XML
+  response_templates = {
+    "application/xml" = <<EOF
+#set($inputRoot = $input.path('$'))
+<?xml version="1.0" encoding="UTF-8"?>
+<message>
+    $inputRoot.body
+</message>
+EOF
+  }
 }
 
 resource "aws_api_gateway_deployment" "api-gateway-vpc-learning-deployment" {
