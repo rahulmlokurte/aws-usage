fdba6b52cb111b582207db30b2bc540d352b89fe
Lokurte, Rahul <rahul.m.lokurte@gmail.com>
2021-09-14 00:02:47 +0530

    Fix the rest api ID

--- terraform/vpc-learning/apigateway.tf
+++ terraform/vpc-learning/apigateway.tf
@@ -39,7 +39,7 @@ EOF
 resource "aws_api_gateway_resource" "api-gateway-vpc-learning-resources" {
   parent_id = aws_api_gateway_rest_api.api-gateway-vpc-learning.id
   path_part = "greet"
-  rest_api_id = aws_api_gateway_rest_api.api-gateway-vpc-learning.id
+  rest_api_id = aws_api_gateway_rest_api.api-gateway-vpc-learning.root_resource_id
 }
 
 resource "aws_api_gateway_method" "api-gateway-vpc-learning-method" {
